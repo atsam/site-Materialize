64d6e1397020c11ab4da93bd5c0bd47a0693e914
atsam <nole.nsonde@gmail.com>
2018-02-14 14:48:46 +0100

    home/region/maison/about

---
+++ js/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.button-collapse').sideNav();
+    $('.slider').slider();
+
+// Pause slider
+$('.slider').slider('pause');
+// Start slider
+$('.slider').slider('start');
+// Next slide
+$('.slider').slider('next');
+// Previous slide
+$('.slider').slider('prev');
+  });
---
+++ js/maison.js
@@ -0,0 +1,16 @@
+  $(document).ready(function(){
+       $('.parallax').parallax();
+     });
+     $(document).ready(function(){
+       $('.button-collapse').sideNav();
+         $('.slider').slider();
+
+     // Pause slider
+     $('.slider').slider('pause');
+     // Start slider
+     $('.slider').slider('start');
+     // Next slide
+     $('.slider').slider('next');
+     // Previous slide
+     $('.slider').slider('prev');
+       });
